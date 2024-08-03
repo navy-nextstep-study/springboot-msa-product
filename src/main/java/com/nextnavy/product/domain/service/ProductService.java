4ff42dbbe8e4cb60a94fc0ea1250de1807a34c02
Yiseul Park <omjl5123@catholic.ac.kr>
2024-08-03 17:42:47 +0900

    comment: TODO 추가

--- src/main/java/com/nextnavy/product/domain/service/ProductService.java
+++ src/main/java/com/nextnavy/product/domain/service/ProductService.java
@@ -21,12 +21,23 @@ public class ProductService {
 	}
 
 	public Product getProduct(final Long productId) {
+		/*
+		 TODO: 타사 제품 조회 로직 추가
+		 TODO: 서킷 브레이커 추가
+		 TODO: 유량 제어 추가
+		*/
 		return productRepository.findById(productId)
 			.orElseThrow(() -> new IllegalArgumentException("Product not found. productId: " + productId));
 	}
 
 	@Transactional
 	public void updateStock(final Long productId, final int quantity) {
+		/*
+		 TODO: 주문 취소 이벤트 발생 시 재고 복원 로직 추가
+		 TODO: 결제 취소 이벤트 발생 시 재고 복원 로직 추가
+		 TODO: 취소 이벤트에 대한 중복 처리 방지 로직 추가
+		*/
+		// TODO: 타사 제품 재고 갱신 로직 추가
 		Product product = productRepository.findById(productId)
 			.orElseThrow(() -> new IllegalArgumentException("Product not found. productId: " + productId));
 		product.updateStock(quantity);
